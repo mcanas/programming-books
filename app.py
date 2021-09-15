4ce20268456c9f05f41a09e8f385bb17bd51d3e1
Miguel Canas <mcanas1981@gmail.com>
2021-09-15 10:06:19 -0400

    Adding book model and app menu

--- app.py
+++ app.py
@@ -1,34 +1,83 @@
-# import models file
-# main menu - add, search, view all, analysis, exit
-# add books to database
-# edit books
-# delete books
-# search books
-# data cleaning
-# app loop
-'''Mockup
+import csv
+from datetime import datetime
+from models import Base, Book, engine, session
 
-PROGRAMMING BOOKS
-1) Add book
-2) View all books
-3) Search for a book
+MAIN_MENU = '''\nPROGRAMMING BOOKS
+1) Add a New Book
+2) View All Books
+3) Search for a Book
 4) Book Analysis
 5) Exit
-
-What would you like to do? 1
-
-Add a New Book
-Title: My Book
-Author: Miguel Canas
-Published (Example: Januar 1, 2021): September 14, 2021
-Price (Example: 10.99): 15.99
-Book Added!
-
-PROGRAMMING BOOKS
-1) Add book
-2) View all books
-3) Search for a book
-4) Book Analysis
-5) Exit
-
 '''
+
+
+# main menu - add, search, view all, analysis, exit
+def menu(choice=0):
+    if choice == 0:
+        print(MAIN_MENU)
+        choice = int(input(f'\nWhat would you like to do? '))
+        return menu(choice)
+    elif choice == 1:
+        print('Add a New Book')
+        title = input('Title: ')
+        author = input('Author: ')
+        date_published = input('Date published: ')
+        price = input('Price: ')
+        # add_book(title, author, date_published, price)
+        print('Book added!')
+    elif choice == 5:
+        print('exit!')
+        return False
+    return True
+
+
+# add books to database
+def add_book(title, author, date_published, price):
+    new_book = Book(title=title, author=author,
+                    date_published=date_published, price=price)
+    session.add(new_book)
+    session.commit()
+
+
+# edit books
+def edit_book():
+    pass
+
+
+# delete books
+def delete_book():
+    pass
+
+
+# search books
+def search_book(id):
+    pass
+
+
+# data import
+def import_books(filepath):
+    with open(filepath, newline='') as csvfile:
+        reader = csv.DictReader(csvfile, fieldnames=(
+            'title', 'author', 'date_published', 'price'))
+        rows = list(reader)
+        for row in rows:
+            add_book(**clean_row(row))
+
+
+# data cleaning
+def clean_row(fields):
+    fields['date_published'] = datetime.strptime(
+        fields['date_published'], '%B %d, %Y').date()
+    fields['price'] = float(fields['price'])
+    return fields
+
+
+# app loop
+def start():
+    Base.metadata.create_all(engine)
+    while menu():
+        continue
+
+
+if __name__ == '__main__':
+    start()
--- models.py
+++ models.py
@@ -1,8 +1,21 @@
-# create a database
-#   books.db
+from sqlalchemy import create_engine, Column, Date, Float, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
-# create a Books model
-#   title
-#   author
-#   date published
-#   price
+engine = create_engine('sqlite:///books.db')
+Session = sessionmaker(bind=engine)
+session = Session()
+Base = declarative_base()
+
+
+class Book(Base):
+    __tablename__ = 'books'
+
+    id = Column(Integer, primary_key=True)
+    title = Column(String)
+    author = Column(String)
+    date_published = Column(Date)
+    price = Column(Float)
+
+    def __repr__(self):
+        return f'<User(title={self.title} author={self.author} date_published={self.date_published} price={self.price})>'
